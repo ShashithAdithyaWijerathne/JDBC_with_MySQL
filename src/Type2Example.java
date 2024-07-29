29df92317b7b100c6bf92ee996ea89afcd1cd865
Adithya Wijerathne <48553417+ShashithAdithyaWijerathne@users.noreply.github.com>
2024-07-29 09:40:12 +0530

    Type 02

--- src/Type2Example.java
+++ src/Type2Example.java
@@ -1,22 +1,20 @@
-//Type 1 example for understanding purpose only (not commonly used now)
-// 01 - Import the libraries into the class
+// This is a conceptual example as Type 2 drivers are specific to database vendors
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-public class Type1Example {
+public class Type2Example {
     public static void main(String[] args) {
         try {
             // 02 - Load the MySQL JDBC Driver
-            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            Class.forName("com.ibm.db2.jcc.DB2Driver");
 
             // 03 - Establish the connection
             Connection connection = DriverManager.getConnection(
-                    "jdbc:odbc:myDataSource", "username", "password");
+                    "jdbc:db2://localhost:50000/myDb", "username", "password");
 
             if (connection != null) {
-                System.out.println("Connected to the database using Type 1 driver!");
+                System.out.println("Connected to the database using Type 2 driver!");
             }
 
             // 04 - Close the connection
